23aba58c6a2b1d8f7ddc145969d2bcef8e23c770
Rafael George <george.rafael@gmail.com>
2011-11-11 21:25:32 -0530

    I'm just fucking drunk and fix comments specs without running it, sounds good I hope.

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -6,41 +6,43 @@ describe "Ticketmaster::Provider::Rally::Comment" do
     @ticket_id = 2780205298
     @comment_id = 2988719307
     @comment_author = "sfw@simeonfosterwillbanks.com"
-  end
-
-  before(:each) do
     @ticketmaster = TicketMaster.new(:rally, {:url => 'https://community.rallydev.com/slm', 
                                               :username => 'ticketmaster-rally@simeonfosterwillbanks.com', 
                                               :password => 'Password'})
-    @project = @ticketmaster.project(@project_id)
+    VCR.use_cassete('get_projects') { @project = @ticketmaster.project(@project_id) }
+    VCR.use_cassette('get_tickets') { @ticket = @project.ticket(:id => @ticket_id) }
+  end
+
+  before(:each) do
     @klass = TicketMaster::Provider::Rally::Comment
-    @ticket = @project.ticket(:id => @ticket_id)
   end
 
   it "should be able to load all comments" do
+  VCR.use_cassete('load_all_rally_comments') do 
     @ticket.comments.should be_an_instance_of(Array)
     @ticket.comments.first.should be_an_instance_of(@klass)
   end
+  end
   
   it "should be able to load all comments based on array of ids" do 
-    comments = @ticket.comments([@comment_id])
-    comments.should be_an_instance_of(Array)
-    comments.first.should be_an_instance_of(@klass)
-    comments.first.author.should == @comment_author
+    VCR.use_cassette('load_comments_by_ids') { @comments = @ticket.comments([@comment_id]) }
+    @comments.should be_an_instance_of(Array)
+    @comments.first.should be_an_instance_of(@klass)
+    @comments.first.author.should == @comment_author
   end
 
   it "should be able to load all comments based on attributes" do 
-    comments = @ticket.comments(:id => @comment_id)
-    comments.should be_an_instance_of(Array)
-    comments.first.should be_an_instance_of(@klass)
-    comments.first.author.should == @comment_author
+    VCR.use_cassette('load_comments_by_attributes') { @comments = @ticket.comments(:id => @comment_id) }
+    @comments.should be_an_instance_of(Array)
+    @comments.first.should be_an_instance_of(@klass)
+    @comments.first.author.should == @comment_author
   end
    
   it "should be able to create a new comment" do
     # Add discussion for User Story US8: Order picture package
-    ticket = @project.ticket(:id => 2712836091)
-    comment = ticket.comment!(:body => 'Pictures will be available for purchase!')
-    comment.should be_an_instance_of(@klass)
+    VCR.use_cassette('create_ticket') { @ticket = @project.ticket(:id => 2712836091) }
+    @comment = ticket.comment!(:body => 'Pictures will be available for purchase!')
+    @comment.should be_an_instance_of(@klass)
   end 
    
 end
