e3decee939561e8960336ed76c7118a3a9bcfc6c
Rafael George <george.rafael@gmail.com>
2011-11-14 18:13:21 -0400

    added vcr use for comments specs

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -6,11 +6,14 @@ describe "Ticketmaster::Provider::Rally::Comment" do
     @ticket_id = 2780205298
     @comment_id = 2988719307
     @comment_author = "sfw@simeonfosterwillbanks.com"
-    @ticketmaster = TicketMaster.new(:rally, {:url => 'https://community.rallydev.com/slm', 
-                                              :username => 'ticketmaster-rally@simeonfosterwillbanks.com', 
-                                              :password => 'Password'})
-    VCR.use_cassete('get_projects') { @project = @ticketmaster.project(@project_id) }
-    VCR.use_cassette('get_tickets') { @ticket = @project.ticket(:id => @ticket_id) }
+    VCR.use_cassette('ticketmaster_setting') do 
+      @ticketmaster = TicketMaster.new(:rally, {:url => 'https://community.rallydev.com/slm', 
+                                       :username => 'ticketmaster-rally@simeonfosterwillbanks.com', 
+                                       :password => 'Password'})
+
+      @project = @ticketmaster.project(@project_id) 
+      @ticket = @project.ticket(:id => @ticket_id) 
+    end
   end
 
   before(:each) do
@@ -18,12 +21,12 @@ describe "Ticketmaster::Provider::Rally::Comment" do
   end
 
   it "should be able to load all comments" do
-  VCR.use_cassete('load_all_rally_comments') do 
-    @ticket.comments.should be_an_instance_of(Array)
-    @ticket.comments.first.should be_an_instance_of(@klass)
+    VCR.use_cassette('load_all_rally_comments') do 
+      @ticket.comments.should be_an_instance_of(Array)
+      @ticket.comments.first.should be_an_instance_of(@klass)
+    end
   end
-  end
-  
+
   it "should be able to load all comments based on array of ids" do 
     VCR.use_cassette('load_comments_by_ids') { @comments = @ticket.comments([@comment_id]) }
     @comments.should be_an_instance_of(Array)
@@ -37,12 +40,12 @@ describe "Ticketmaster::Provider::Rally::Comment" do
     @comments.first.should be_an_instance_of(@klass)
     @comments.first.author.should == @comment_author
   end
-   
+
   it "should be able to create a new comment" do
     # Add discussion for User Story US8: Order picture package
-    VCR.use_cassette('create_ticket') { @ticket = @project.ticket(:id => 2712836091) }
-    @comment = ticket.comment!(:body => 'Pictures will be available for purchase!')
+    VCR.use_cassette('retrieve_ticket')  {@ticket = @project.ticket(:id => 2712836091) }
+    VCR.use_cassette('create_comment') { @comment = @ticket.comment!(:body => 'Pictures will be available for purchase!') }
     @comment.should be_an_instance_of(@klass)
   end 
-   
+
 end
